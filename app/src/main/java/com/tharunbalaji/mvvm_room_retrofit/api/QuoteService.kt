c5cdfd8ed6672def9e13248fa4b5ce3e89f04420
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-07-15 12:20:19 +0530

    Retrofit setup

---
+++ app/src/main/java/com/tharunbalaji/mvvm_room_retrofit/api/QuoteService.kt
@@ -0,0 +1,11 @@
+package com.tharunbalaji.mvvm_room_retrofit.api
+
+import com.tharunbalaji.mvvm_room_retrofit.models.QuoteList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface QuoteService {
+    @GET("/quotes")
+    suspend fun getQuotes(@Query("page") page: Int): Response<QuoteList>
+}
---
+++ app/src/main/java/com/tharunbalaji/mvvm_room_retrofit/api/RetrofitHelper.kt
@@ -0,0 +1,15 @@
+package com.tharunbalaji.mvvm_room_retrofit.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitHelper {
+    private const val BASE_URL = "https://quotable.io/"
+
+    fun getInstance(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
