afacd1e66a857daddb74d46030e44a3abccacd5a
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-07-15 12:16:23 +0530

    Added Model Class

---
+++ app/src/main/java/com/tharunbalaji/mvvm_room_retrofit/models/QuoteList.kt
@@ -0,0 +1,10 @@
+package com.tharunbalaji.mvvm_room_retrofit.models
+
+data class QuoteList(
+    val count: Int,
+    val lastItemIndex: Int,
+    val page: Int,
+    val results: List<Result>,
+    val totalCount: Int,
+    val totalPages: Int
+)
